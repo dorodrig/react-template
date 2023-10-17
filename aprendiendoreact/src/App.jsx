a8569988c7669bebd7d1c95e9155e1a1676875d8
David Orlando Rodriguez Bastidas <David.Rodriguez@GRMCOLOMBIA.com>
2023-10-17 07:44:27 -0500

    codigo javascript con html para react

--- aprendiendoreact/src/App.jsx
+++ aprendiendoreact/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
 
 function App() {
+  const nombre = 'David Rodriguez';
+  const edad = 24;
+
   return (
     <div className='app'>
       <h1>¡Hola, mundo!</h1>
       <p>aca estamos mirando que con jsx puedo agregar codigo html y css con react</p>
+      <p>Mi nombre es {nombre}</p>
+      {/* esto es un if en jsx */}
+      <p>{edad> 18 ?"adulto":"Niño"}</p>
     </div>
   )
 }
