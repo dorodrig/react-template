fac9b71f5c69cdb853c952019b4e1e6b8ad4f697
dorodrig <dorodriguez145@misena.edu.co>
2023-11-06 21:27:18 -0500

    React-redux: se incluye en el componente caritems la informacion del slice y queda sin (scss)

--- redux-toolkit-shopping-cart/src/components/cart/CartItem.jsx
+++ redux-toolkit-shopping-cart/src/components/cart/CartItem.jsx
@@ -1,16 +1,24 @@
 import PropTypes from "prop-types";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 const CartItem = ({ id, title, prices, img, amount }) => {
   return (
-    <>
-      <ul>
-        <li>{id}</li>
-        <li>{title}</li>
-        <li>{prices}</li>
-        <li>{img}</li>
-        <li>{amount}</li>
-      </ul>
-      <hr />
-    </>
+    <article key={id} className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h5>{prices}</h5>
+        <button className="cart-item__remove">remove</button>
+      </div>
+      <div>
+        <button className="cart-item__amount-btn">
+          <FaChevronUp />
+        </button>
+        <p className="cart-item__amount-btn-text">{amount}</p>
+        <button className="cart-item__amount-btn">
+          <FaChevronDown />
+        </button>
+      </div>
+    </article>
   );
 };
 CartItem.propTypes = {
