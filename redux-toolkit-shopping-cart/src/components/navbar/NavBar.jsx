c0e2a8bbba2de25550e04809058784938a46d9cb
dorodrig <dorodriguez145@misena.edu.co>
2023-11-06 19:20:40 -0500

    React-redux: styles header

--- redux-toolkit-shopping-cart/src/components/navbar/NavBar.jsx
+++ redux-toolkit-shopping-cart/src/components/navbar/NavBar.jsx
@@ -1,12 +1,15 @@
 //instalar la libreria de iconos npm install react-icons
 import { GrCart } from "react-icons/gr";
+import { useSelector } from "react-redux";
 const NavBar = () => {
+  //obtiene el valor de un state
+  const { amount } = useSelector((store) => store.cart);
   return (
     <header>
-      <h1>My store Bekia</h1>
+      <h1>My store Bekia Cosmetics</h1>
       <div className="amount-container">
         <GrCart className="cart-icon" />
-        <span className="badge">4</span>
+        <span className="badge">{amount}</span>
       </div>
     </header>
   );
